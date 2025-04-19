486e95b6283d5c2cbe67c477232834f501e664ff
texascake <budalblimbing@gmail.com>
2025-04-19 23:50:38 +0700

    Update keys.mk

--- keys.mk
+++ keys.mk
@@ -1,1 +1,9 @@
+ifneq (eng,$(TARGET_BUILD_VARIANT))
+ifneq (,$(wildcard vendor/lineage-priv/keys/releasekey.pk8))
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.oem_unlock_supported=1
+endif
+ifneq (,$(wildcard vendor/lineage-priv/keys/otakey.x509.pem))
+PRODUCT_OTA_PUBLIC_KEYS := vendor/lineage-priv/keys/otakey.x509.pem
+endif
+endif
